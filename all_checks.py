8f84ff0b57fc443637f56bf10482ff89570dabf9
cwongsecondary <cwongsecondary@gmail.com>
2022-11-28 14:31:44 -0500

    Added a simple network connectivity check

--- all_checks.py
+++ all_checks.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-import os, shutil, sys
+import os, shutil, sys, socket
+
 
 def check_reboot():
     """Returns True if the computer has a pending reboot"""
@@ -26,10 +27,20 @@ def check_root_full():
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
 
+def check_no_network():
+    """Returns True if it fails to resolve Google's URL, False otherwise"""
+    try:
+        socket.gethostbyname("www.google.com")
+        return False
+    except:
+        return True
+
+
 def main():
     checks = [
         (check_reboot, "Pending Reboot"),
         (check_root_full,"Root partition full"),
+        (check_no_network, "No working Network"),
     ]
 
     # Show more than 1 message IF more than 1 Check Failed via use of a Boolean Variable
